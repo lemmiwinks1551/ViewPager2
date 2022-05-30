1ed50480a2b4978b2371bdd4c2e308e7de8818c7
kirill <ananevka@mail.ru>
2022-05-30 23:28:37 +0300

    start

---
+++ app/src/main/java/com/example/viewpager2/PageFragment.java
@@ -0,0 +1,42 @@
+package com.example.viewpager2;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+public class PageFragment extends Fragment {
+
+    private int pageNumber; // Номер текущей страницы.
+
+    public static PageFragment newInstance(int page) {
+        PageFragment fragment = new PageFragment();
+        Bundle args = new Bundle();
+        args.putInt("num", page);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public PageFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        pageNumber = getArguments() != null ? getArguments().getInt("num") : 1;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View result=inflater.inflate(R.layout.fragment_page, container, false);
+        TextView pageHeader = result.findViewById(R.id.displayText);
+        String header = "Фрагмент " + (pageNumber+1);
+        pageHeader.setText(header);
+        return result;
+    }
+
+}
