896f20d2c946bf96e601dc7479cbede315e3f2a4
kirill <ananevka@mail.ru>
2022-05-31 13:20:11 +0300

    v.0

--- app/src/main/java/com/example/viewpager2/MainActivity.java
+++ app/src/main/java/com/example/viewpager2/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.viewpager2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager2 pager = findViewById(R.id.pager);
+        FragmentStateAdapter pageAdapter = new MyAdapter(this);
+        pager.setAdapter(pageAdapter);
     }
 }
